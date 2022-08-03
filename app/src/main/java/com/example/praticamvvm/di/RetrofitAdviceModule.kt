2e284aa24b5c2dd1e41ce66e464269b4f2c004cb
fabianamf <fabianamf@ciandt.com>
2022-08-03 12:49:34 -0300

    creating module

--- app/src/main/java/com/example/praticamvvm/di/RetrofitAdviceModule.kt
+++ app/src/main/java/com/example/praticamvvm/di/RetrofitAdviceModule.kt
@@ -1,2 +1,15 @@
 package com.example.praticamvvm.di
 
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val retrofitAdviceModule = module {
+    single<Retrofit> {
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://api.adviceslip.com/advice")
+            .build()
+    }
+
+}
